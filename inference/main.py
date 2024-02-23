e44511a821e8a472db1d05f3d5742fa2e37b1afc
Florian Bosse <bosse@autop.de>
2024-02-23 09:56:23 +0100

    new version

--- inference/main.py
+++ inference/main.py
@@ -71,7 +71,7 @@ print("Done preprocessing")
 
 app = Flask(__name__)
 api = Api(app, 
-          version='0.3', 
+          version='0.4', 
           title='Sentiment Inference API', 
           description='API that demos how to infer the sentiment out of a sentence')
 
